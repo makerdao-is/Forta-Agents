3c961fa5ce3fe78becda42fbcdc61ddf20e37084
Joshua N. Pritikin <joshua@is.makerdao.network>
2022-08-02 12:02:50 -0700

    Update spell deployer

--- Maker-Agents/Governance-Module/src/config.ts
+++ Maker-Agents/Governance-Module/src/config.ts
@@ -1,7 +1,7 @@
 import { BigNumber } from "ethers";
 
 const CHAINLOG_CONTRACT: string = "0xda0ab1e0017debcd72be8599041a2aa3ba7e740f";
-const SPELL_DEPLOYER: string = "0xda0c0de01d90a5933692edf03c7ce946c7c50445";
+const SPELL_DEPLOYER: string = "0xDa0c0De020F80d43dde58c2653aa73d28Df1fBe1";
 const KNOWN_LIFTERS: string[] = ["0x5cab1e5286529370880776461c53a0e47d74fb63"];
 const MKR_THRESHOLD: BigNumber = BigNumber.from(40000);
 
