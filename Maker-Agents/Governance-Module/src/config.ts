6c84f9f07b47138ad99b027819ec432ce873b233
Joshua N. Pritikin <joshua@is.makerdao.network>
2022-10-24 10:49:59 -0700

    Update based on addrs used for exec 0x8E4faFef5bF61f09654aDeB46E6bC970BcD42c52

--- Maker-Agents/Governance-Module/src/config.ts
+++ Maker-Agents/Governance-Module/src/config.ts
@@ -1,8 +1,8 @@
 import { BigNumber } from "ethers";
 
 const CHAINLOG_CONTRACT: string = "0xda0ab1e0017debcd72be8599041a2aa3ba7e740f";
-const SPELL_DEPLOYER: string = "0xDa0c0De020F80d43dde58c2653aa73d28Df1fBe1";
-const KNOWN_LIFTERS: string[] = ["0x5cab1e5286529370880776461c53a0e47d74fb63"];
+const SPELL_DEPLOYER: string = "0xC1E6d8136441FC66612Df3584007f7CB68765e5D";
+const KNOWN_LIFTERS: string[] = ["0x8b4c184918947b52f615FC2aB350e092906b54CB"];
 const MKR_THRESHOLD: BigNumber = BigNumber.from(40000);
 
 export default {
